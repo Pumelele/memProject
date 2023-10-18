69ea8e99b666c23de46d77faa2a773d19ddf17c3
Pumelele <pumelele.qata01@gmail.com>
2023-10-18 14:45:56 +0200

    Edited index message

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
+
+
 const app = express();
 
 
@@ -14,6 +16,10 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+
+app.get( (req,res) => { 
+    res.send('SERVER IS RUNNING')
+})
 
 //connect with database: mongodb
 const CONNECTION_URL = "mongodb+srv://nodeconnect:nodeconnect_123@cluster0.cs7zb7q.mongodb.net/";
